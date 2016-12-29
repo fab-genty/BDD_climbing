7ffcc1c11a2f8b04e54eff16a309add06ea88234
fab-genty <electronsource75@gmail.com>
2016-12-29 17:52:56 +0100

    Create requete4.sql

---
+++ requete4.sql
@@ -0,0 +1,3 @@
+--La liste des clients ayant participé à au moins une réunion par mois dans les douze derniers mois.
+--Impossible de prenndre au moins une réunion par mois car pas assez de compétitions par ans.
+--On va donc prendre les compétitions sur une année et pour toute les disciplines et faire le compte
