58979158110bd38923a59ba8af7b53ac99329194
fab-genty <electronsource75@gmail.com>
2016-12-29 17:52:13 +0100

    Create requete3.sql

---
+++ requete3.sql
@@ -0,0 +1,4 @@
+--Les clients ayant participé le mois dernier plus que deux fois la moyenne générale de participations
+--par client pendant cette période.On va prendre comme durée 1 an et pas 1 mois Dans notre cas  les
+--sportifs qui ont participe cette année en moyenne deux fois aux meeting que les autres .
+--Donc les participants dont la moyenne est 2 fois  > a celle des autres participants
